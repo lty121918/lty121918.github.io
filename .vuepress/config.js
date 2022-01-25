74f942ab8d84c4b8ffac8566dddf979b0748c163
‘zhouzi’ <zhouzi@gaoding.com>
2022-01-25 19:12:23 +0800

    111

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,110 +1,107 @@
 module.exports = {
-  "title": "",
-  "description": "",
-  "dest": "public",
-  "base":"/blog/",
-  "head": [
-    [
-      "link",
-      {
-        "rel": "icon",
-        "href": "/favicon.ico"
-      }
-    ],
-    [
-      "meta",
-      {
-        "name": "viewport",
-        "content": "width=device-width,initial-scale=1,user-scalable=no"
-      }
-    ]
-  ],
-  "theme": "reco",
-  "themeConfig": {
-    "nav": [
-      {
-        "text": "Home",
-        "link": "/",
-        "icon": "reco-home"
-      },
-      {
-        "text": "TimeLine",
-        "link": "/timeline/",
-        "icon": "reco-date"
-      },
-      {
-        "text": "Docs",
-        "icon": "reco-message",
-        "items": [
-          {
-            "text": "vuepress-reco",
-            "link": "/docs/theme-reco/"
-          },
-          {
-            "text": "JavaScript",
-            "link": "/docs/JavaScript"
-          },
-          {
-            "text": "网络相关",
-            "link": "/docs/Network/"
-          }
+    "title": "", //博客的标题
+    "description": "", //博客的描述
+    "dest": "public", //博客部署时输出的文件夹
+    "base": "/blog/",
+    "head": [
+        [
+            "link",
+            {
+                "rel": "icon",
+                "href": "/favicon.ico" //favicon图标设置
+            }
+        ],
+        [
+            "meta",
+            {
+                "name": "viewport",
+                "content": "width=device-width,initial-scale=1,user-scalable=no"
+            }
         ]
-      },
-      {
-        "text": "Contact",
-        "icon": "reco-message",
-        "items": [
-          {
-            "text": "GitHub",
-            "link": "https://github.com/recoluan",
-            "icon": "reco-github"
-          }
-        ]
-      }
     ],
-    "sidebar": {
-      "/docs/theme-reco/": [
-        "",
-        "theme",
-        "plugin",
-        "api"
-      ]
+    "theme": "reco", //vuepress挂载的主题
+    "themeConfig": {
+        //导航栏
+        "nav": [{
+                "text": "Home",
+                "link": "/",
+                "icon": "reco-home"
+            },
+            {
+                "text": "TimeLine",
+                "link": "/timeline/",
+                "icon": "reco-date"
+            },
+            {
+                "text": "Docs",
+                "icon": "reco-message",
+                "items": [{
+                        "text": "vuepress-reco",
+                        "link": "/docs/theme-reco/"
+                    },
+                    {
+                        "text": "JavaScript",
+                        "link": "/docs/JavaScript/"
+                    },
+                    {
+                        "text": "网络相关",
+                        "link": "/docs/Network/"
+                    }
+                ]
+            },
+            {
+                "text": "Contact",
+                "icon": "reco-message",
+                "items": [{
+                    "text": "GitHub",
+                    "link": "https://github.com/recoluan",
+                    "icon": "reco-github"
+                }]
+            }
+        ],
+        //侧边栏
+        "sidebar": {
+            "/docs/theme-reco/": [
+                "",
+                "theme",
+                "plugin",
+                "api"
+            ]
+        },
+        "type": "blog",
+        "blogConfig": {
+            "category": {
+                "location": 2,
+                "text": "Category"
+            },
+            "tag": {
+                "location": 3,
+                "text": "Tag"
+            }
+        },
+        "friendLink": [{
+                "title": "午后南杂",
+                "desc": "Enjoy when you can, and endure when you must.",
+                "email": "1156743527@qq.com",
+                "link": "https://www.recoluan.com"
+            },
+            {
+                "title": "vuepress-theme-reco",
+                "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+                "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+                "link": "https://vuepress-theme-reco.recoluan.com"
+            }
+        ],
+        "logo": "/logo.png",
+        "search": true,
+        "searchMaxSuggestions": 10,
+        "lastUpdated": "Last Updated",
+        "author": "",
+        "authorAvatar": "/avatar.png",
+        "record": "xxxx",
+        "startYear": "2017"
     },
-    "type": "blog",
-    "blogConfig": {
-      "category": {
-        "location": 2,
-        "text": "Category"
-      },
-      "tag": {
-        "location": 3,
-        "text": "Tag"
-      }
-    },
-    "friendLink": [
-      {
-        "title": "午后南杂",
-        "desc": "Enjoy when you can, and endure when you must.",
-        "email": "1156743527@qq.com",
-        "link": "https://www.recoluan.com"
-      },
-      {
-        "title": "vuepress-theme-reco",
-        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        "link": "https://vuepress-theme-reco.recoluan.com"
-      }
-    ],
-    "logo": "/logo.png",
-    "search": true,
-    "searchMaxSuggestions": 10,
-    "lastUpdated": "Last Updated",
-    "author": "",
-    "authorAvatar": "/avatar.png",
-    "record": "xxxx",
-    "startYear": "2017"
-  },
-  "markdown": {
-    "lineNumbers": true
-  }
+    "markdown": {
+        "lineNumbers": true
+    }
 }
--- docs/JavaScript/JavaScript.md
+++ docs/JavaScript/JavaScript.md
@@ -1,6 +1,7 @@
 ---
 title: asdasd
 date: 2021-09-21
+sidebar: 'auto'
 tags:
  - 数组
 categories:
