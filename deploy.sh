c971162bee726990059947edaae5e4fe7e3009c6
Your Name <1219186777@qq.com>
2022-05-21 16:57:52 +0800

    1213'

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run build
 
 # 进入生成的文件夹
-# cd docs/.vuepress/public
+cd public
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -17,9 +17,9 @@ git add .
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:lty123/lty123.github.io.git master
+git push -f git@github.com:lty121918/lty121918.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:lty121918/blog.git master:gh-pages
+# git push -f git@github.com:lty121918/blog.git master:gh-pages
 
 cd -
